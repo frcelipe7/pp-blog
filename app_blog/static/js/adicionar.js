0e7ec31e0f58acea5a6fc05b1c4224b2b3f8e098
felipe raphael santos <feliperaphaelpara@gmail.com>
2022-07-21 18:43:05 -0300

    adicionar.js

--- app_blog/static/js/adicionar.js
+++ app_blog/static/js/adicionar.js
@@ -24,4 +24,32 @@ document.addEventListener("DOMContentLoaded", () => {
             error.setAttribute('reverse', '');
         });
     } catch (e) {}
+
+    const quebrar_linha = document.getElementById("quebrar-linha-demo");
+    quebrar_linha.innerText = 'texto<br>';
+
+    const subtema = document.getElementById("subtema-demo");
+    subtema.innerText = '# texto';
+
+    const negrito = document.getElementById("negrito");
+    negrito.innerText = '**texto**';
+
+    const italico = document.getElementById("italico");
+    italico.innerText = '*texto*';
+
+    const citacao = document.getElementById("citacao");
+    citacao.innerText = '> texto';
+
+    const link = document.getElementById("link");
+    link.innerText = '[Texto aqui](https://Link-aqui)';
+
+    const preview_button = document.getElementById("preview-button");
+    const preview_content = document.querySelector(".preview-content .preview");
+    preview_button.addEventListener("click", () => {
+        document.querySelector(".preview-content").style.display = 'block';
+        var converter = new showdown.Converter();
+        var text = document.getElementById("text").value;
+        var html = converter.makeHtml(text);
+        preview_content.innerHTML = html;
+    });
 });
